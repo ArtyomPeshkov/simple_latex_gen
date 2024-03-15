fe29666ae4d14e05a0c4d9f1c986ceadfd238247
Artyom Peshkov <artemyipeshkov@gmail.com>
2024-03-15 21:55:58 +0300

    fix

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='simple_latex_gen',
+    version='0.1.0',
+    description='Primitive latex generator',
+    long_description='Primitive latex generator',
+    author='Artyom',
+    author_email='artemyipeshkov@gmail.com',
+    url='https://github.com/ArtyomPeshkov/simple_latex_gen',
+    packages=['simple_latex_gen'],
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
